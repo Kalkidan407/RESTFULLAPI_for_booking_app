9b18903438ca08e4eb45d54a68c53dfc17fb83a5
Kalkidan407 <kelemework267@gmail.com>
2025-08-17 11:43:41 +0100

    user entity updated

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +13,6 @@ public class User {
 
     private String name;
     private String email;
+    private String phone;
+    private String role; // CUSTOMER, DESIGNER, ADMIN
 }
