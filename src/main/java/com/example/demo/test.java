127ec0c7f598068dcea5557c8d71e3306e9eac61
Kalkidan407 <kelemework267@gmail.com>
2025-08-17 14:46:42 +0100

    controller updated

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -1,51 +1,7 @@
-
-// // User.java
-// @Entity
-// @Table(name = "users")
-// public class User {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-
-//     private String name;
-//     private String email;
-//     private String phone;
-//     private String role; // CUSTOMER, DESIGNER, ADMIN
-// }
-// Service.java
+// spring.datasource.url=jdbc:mysql://localhost:3306/graphic_design_db
+// spring.datasource.username=root
+// spring.datasource.password=yourpassword
+// spring.jpa.hibernate.ddl-auto=update
+// spring.jpa.show-sql=true
 
 
-
-// Service.java
-// @Entity
-// @Table(name = "services")
-// public class Service {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-
-//     private String name;
-//     private String description;
-//     private Double price;
-// }
-
-// Order.java
-// @Entity
-// @Table(name = "orders")
-// public class Order {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-
-//     @ManyToOne
-//     @JoinColumn(name = "customer_id")
-//     private User customer;
-
-//     @ManyToOne
-//     @JoinColumn(name = "service_id")
-//     private Service service;
-
-//     private String status; // PENDING, IN_PROGRESS, COMPLETED
-//     private LocalDate orderDate;
-//     private LocalDate deliveryDate;
-// }
